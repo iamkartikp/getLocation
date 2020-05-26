715a5aed20114de384bf9464094b06c282ad62e8
Kartik <55845219+iamkartikp@users.noreply.github.com>
2020-05-26 22:14:01 +0530

    changed UI, added timestamp

--- JS/home.js
+++ JS/home.js
@@ -2,8 +2,16 @@ let displayLocation = document.querySelector('#displayLocation');
 let btn = document.querySelector('#btn');
 let inputImage = document.querySelector('#inputImage');
 let image = document.querySelector('#output');
+let caption = document.querySelector('#caption');
+let displayCaption = document.querySelector('#displayCaption');
+let displayDate = document.querySelector('#displayDate');
+
+let d = new Date();
+let now = d.toLocaleTimeString();
 
 btn.addEventListener('click', ()=> {
+    displayCaption.textContent = caption.value;
+    displayDate.textContent = `Posted at: ${now}`;
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(myCurrentLocation);
     } else {
